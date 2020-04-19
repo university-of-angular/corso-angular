6bda0cc78283af8c0ded644b4d4eb6c9fb83307a
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-04-19 10:11:32 +0200

    Angular built-pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,12 @@ import { Course } from './components/course-card/model/course';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Core Deep Dive';
   courses = COURSES;
+  title = COURSES[1].description;
+  startDate = new Date(2000, 0, 1);
+  price = 9.99;
+  rate = 0.67;
+  course = COURSES[1];
 
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled...', course);
